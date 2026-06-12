4fa8e9928443c5b074347df33a1e878e57fd16a6
kimrunan <krandreassen@gmail.com>
2026-06-12 19:33:06 +0200

    nyheter

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
-// 1) Lim inn Web App URL fra Google Apps Script her.
-// Eksempel: https://script.google.com/macros/s/AKfy.../exec
 const APP_CONFIG = {
   appsScriptUrl: "DIN_APPS_SCRIPT_WEB_APP_URL_HER",
-
-  // 2) Publisert CSV fra Google Sheets.
-  // Arket bør ha kolonnene: Timestamp, Plass, Navn, Kontakt, Fra, Til
   bookingsCsvUrl: "DIN_PUBLISERTE_CSV_URL_HER",
-
-  // Oppdaterer status fra Google Sheets hvert 10. sekund.
   refreshIntervalMs: 10000,
-
-  // Sett til false hvis du vil skjule fullt navn i kart/info.
-  showFullName: true
+  adminUser: "admin",
+  adminPassword: "Hyttevold2026",
+  bookingEnabled: true,
+  bookingOpenText: "Booking er åpen.",
+  bookingClosedText: "Booking er stengt."
 };
---
+++ icons/README.txt
@@ -0,0 +1,1 @@
+Legg egne ikoner her: bobil.svg, campingvogn.svg, telt.svg, bat.svg
