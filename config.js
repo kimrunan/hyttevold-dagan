c8cc2a54923863965ac45f68e97246447e498857
kimrunan <krandreassen@gmail.com>
2026-06-12 20:23:48 +0200

    oppdatert

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const APP_CONFIG = {
   // Ekte booking krever Apps Script URL.
-  appsScriptUrl: "DIN_APPS_SCRIPT_WEB_APP_URL_HER",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzlsArraMIZEb7vZcE8my5qiP2ViPG_paz4A6xVidtBoFv5GFONvwu4Um2mXQoaHfwfSA/exec",
 
   // Publisert CSV fra Google Sheet.
   // Kolonner: Timestamp, Plass, Navn
-  bookingsCsvUrl: "DIN_PUBLISERTE_CSV_URL_HER",
+  bookingsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSWeVLX-_EP_glVBrHjj4FtpaSe0jhZyv06OQPruEiso18V_pIEON82yHUX2S1WE1RckemMIOFx9a-a/pub?output=csv",
 
   refreshIntervalMs: 10000,
 
